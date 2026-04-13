421612d7eb74ba5bddba81ba34b5685d7ad35278
JAIRO-VF <114115899+jairovargasfigueroa@users.noreply.github.com>
2026-04-12 20:38:21 -0400

    first commit

---
+++ src/main/java/com/jairo/workflowtramites/WorkflowTramitesApplication.java
@@ -0,0 +1,13 @@
+package com.jairo.workflowtramites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowTramitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowTramitesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jairo/workflowtramites/WorkflowTramitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jairo.workflowtramites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowTramitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
